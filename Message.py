76475cdf443bdfb40220a6522191c6ad6f7127a8
Zachery.Lange <Zachery.Lange@caveink.com>
2021-06-24 05:27:35 -0500

    Add basic message creation after network simulation, moving is hard

--- Message.py
+++ Message.py
@@ -1,8 +1,10 @@
 from Signature import Signature
 
 class Message:
-    def __init__(self, payload, sender_id, round_sent, signatures: list[Signature] = []):
+    def __init__(self, payload, sender_id, round_sent, signatures: list[Signature] = [], reciver_id = None, is_valid = None):
         self.payload = payload
         self.sender_id = sender_id
         self.round_sent = round_sent
         self.signatures = signatures
+        self.reciver_id = reciver_id
+        self.is_valid = is_valid
--- visualization.py
+++ visualization.py
@@ -1,4 +1,6 @@
+from Node import Node
 import pygame
+import pygame.examples.aliens
 import os
 from bb_consensus_simulation import run_bb_simulation
 
@@ -38,26 +40,77 @@ class Leader(pygame.sprite.Sprite):
 
     def __init__(self, starting_point, is_traitor = False):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.is_traitor = is_traitor
         self.image = self.images[is_traitor] 
         self.rect = self.image.get_rect(topleft=starting_point)
 
 class General(pygame.sprite.Sprite):
     images = []
 
-    def __init__(self, starting_point):
+    def __init__(self, starting_point, is_traitor = False):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.image = self.images[0]
+        self.image = self.images[is_traitor] 
         self.rect = self.image.get_rect(topleft=starting_point)
 
 class Message(pygame.sprite.Sprite):
     images = []
+    speed = 5
 
-    def __init__(self, starting_point):
+    def __init__(self, starting_point, payload, y_distance, is_up, enter_left):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = self.images[0]
         self.rect = self.image.get_rect(topleft=starting_point)
+        self.payload = payload
+        self.leave_start_x = self.rect.right + 100
+        self.leave_start_x_done = False
+        self.y_distance = abs(y_distance)
+        self.y_moved = 0
+        self.is_up = is_up
+        self.y_distance_done = False
+        self.enter_x_value = 100
+        self.enter_x_moved = 0
+        self.enter_left = enter_left
+        self.enter_x_done = False
+        self.step = 0
+        self.font = pygame.font.Font(None, 30)
+        self.color = pygame.Color("Black")
 
+    def update(self):
+        if(self.step == 0):
+            self.exit()
+
+        if(self.step == 1):
+            self.y_change()
+
+        if(self.step == 2):
+            self.enter()
+
+        self.draw_payload()
+
+    def draw_payload(self):
+        self.image.blit(self.font.render(str(self.payload), 0, self.color), (4,8))            
+
+    def exit(self):
+        if(self.rect.right <= self.leave_start_x and not self.leave_start_x_done):
+            self.rect.move_ip(self.speed,0)
+        else: 
+            self.leave_start_x_done = True
+            self.step = 1
+
+    def y_change(self):
+        if(self.y_moved <= self.y_distance):
+            self.y_moved += self.speed
+            self.rect.move_ip(0,(self.speed * self.is_up))
+        else:
+            self.y_distance_done = True
+            self.step = 2
+
+    def enter(self):
+        if(self.enter_x_moved <= self.enter_x_value):
+            self.enter_x_moved += self.speed
+            self.rect.move_ip((self.speed * self.enter_left), 0 )
+        else:
+            self.enter_x_done = True
+            
 class CheckButton(pygame.sprite.Sprite):
     images = []
     checked = FORCE_TRAITOR_LEADER
@@ -111,7 +164,7 @@ class Traitor_count_text_field(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=starting_point)    
 
     def update(self):
-        self.image = self.font.render(str(NUM_OF_TRAITORS), 0, self.color)        
+        self.image = self.font.render(str(NUM_OF_TRAITORS), 0, self.color)
 
 def main():
     # Used for set frames per second
@@ -147,13 +200,13 @@ def main():
     pygame.display.set_icon(icon)
     pygame.display.set_caption("Byzantine General Consensus Problem")
 
-    nodes = pygame.sprite.Group()
-    messages = pygame.sprite.Group()
+    node_group = pygame.sprite.Group()
+    message_group = pygame.sprite.Group()
     all = pygame.sprite.RenderUpdates()
 
-    Leader.containers = nodes, all
-    General.containers = nodes, all
-    Message.containers = messages, all
+    Leader.containers = node_group, all
+    General.containers = node_group, all
+    Message.containers = message_group, all
     CheckButton.containers = all
     Button.containers = all
     Text_field.containers = all
@@ -161,12 +214,12 @@ def main():
     Traitor_count_text_field.containers = all
 
     start_screen(clock, screen, background, arrow, all)
-
+    
     all.clear(screen, background)
     all.empty()
     pygame.display.update()
-
-    bb_replay(clock, screen,background, all)
+    
+    bb_replay(clock, screen,background, all, node_group, message_group)
     
 def start_screen(clock, screen, background, arrow, all):
     buttons = []
@@ -198,16 +251,10 @@ def start_screen(clock, screen, background, arrow, all):
     start_simulation_button = Button((start_simulation_text_field.rect.right + 10, start_simulation_text_field.rect.top), pygame.transform.rotate(arrow, -90), start_simulation_callback)
     buttons.append(start_simulation_button)
 
-    pause = False
     while START_SCREEN:
 
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                pause = not pause
-                print(pause)
-
-            if(pause): break
-
+        
             if event.type == pygame.QUIT:
                 pygame.quit()
             
@@ -216,8 +263,6 @@ def start_screen(clock, screen, background, arrow, all):
                 for button in buttons:
                     if button.rect.collidepoint(x,y):
                         button.callback()
-
-        if(pause): continue
 
         all.clear(screen,background)
 
@@ -228,17 +273,19 @@ def start_screen(clock, screen, background, arrow, all):
 
         clock.tick(FPS)
 
-def bb_replay(clock , screen , background, all):
+def bb_replay(clock , screen , background, all, node_group, message_group):
     
     Text_field((20,20), "Simulating BB Consensus", 75)
 
     all.update()
     pygame.display.update(all.draw(screen))
 
-    round_dict = run_bb_simulation_and_process_output()
+    network = run_bb_simulation(NUM_OF_NODES, NUM_OF_TRAITORS, FORCE_TRAITOR_LEADER)
+    round_dict = run_bb_simulation_and_process_output(network)
 
     pop = load_sound("pop.mp3")
     dink = load_sound("dink.wav")
+    message_out = load_sound("message_out.mp3")
     valid_message = load_sound("valid_message.ogg")
     
     pygame.time.wait(1000)
@@ -247,14 +294,34 @@ def bb_replay(clock , screen , background, all):
     pygame.display.update()
     
     buttons = []
+    nodes = []
+
+    leader = Leader((64, (SCREENRECT.bottom/2) -32), network[0].is_traitor)
+    pygame.display.update(all.draw(screen))
+    pop.play()
+    nodes.append(leader)
+
+    for i in range(1, NUM_OF_NODES):
+        pygame.time.wait(500)
+        general = General((320, (i*104)), network[i].is_traitor)
+        pygame.display.update(all.draw(screen))
+        pop.play()
+        nodes.append(general)
+
+    pygame.time.wait(500)
+    
+    round_count= 1 
+    node_sending = 0
+
+    round_text_field = Text_field((0,0), "Round: ", 70)
+    round_count_text_field = Text_field((round_text_field.rect.right,0), str(round_count) ,70)
+
+    draw_messages(round_count, node_sending, nodes, round_dict, all , screen, message_out)
 
     pause = False
     while REPLAY_SCREEN:
 
         for event in pygame.event.get():
-            # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            #     pause = not pause
-            #     print(pause)
 
             if(pause): break
 
@@ -271,18 +338,37 @@ def bb_replay(clock , screen , background, all):
 
         all.clear(screen,background)
 
-        all.update()
+        round_count_text_field.text = str(round_count)
 
+        all.update()
         dirty = all.draw(screen)
         pygame.display.update(dirty)
 
         clock.tick(FPS)
 
-    
+    pygame.quit()
+
+def draw_messages(round_count, node_sending, nodes, round_dict, all, screen, message_out):
+    messages_to_draw = round_dict[round_count][node_sending]
+
+    for message in messages_to_draw:
+        starting_node = nodes[message.sender_id]
+        ending_node = nodes[message.reciver_id]
+
+        y_distance = (ending_node.rect.top - starting_node.rect.top) + 18
+        is_up = -1 if (y_distance < 0 ) else 1
+        enter_left = 1 if (node_sending == 0) else -1
+        Message((starting_node.rect.right + 1, starting_node.rect.top + 16), message.payload, y_distance, is_up, enter_left)
+
+
+    pygame.display.update(all.draw(screen))
+    message_out.play()   
+        
 
 def force_traitor_leader_checkbox_callback():
-    global FORCE_TRAITOR_LEADER
-    FORCE_TRAITOR_LEADER = not FORCE_TRAITOR_LEADER
+    global FORCE_TRAITOR_LEADER    
+    if(NUM_OF_TRAITORS > 0):
+        FORCE_TRAITOR_LEADER = not FORCE_TRAITOR_LEADER
 
 def num_of_node_increase_callback():
     global NUM_OF_NODES
@@ -310,9 +396,22 @@ def start_simulation_callback():
     START_SCREEN = False
     REPLAY_SCREEN = True
 
-def run_bb_simulation_and_process_output():
-    network = run_bb_simulation(NUM_OF_NODES, NUM_OF_TRAITORS, FORCE_TRAITOR_LEADER)
-    return None   
+def run_bb_simulation_and_process_output(network: dict[Node]):
+
+    round_dict = {}
+
+    for i in range(0, NUM_OF_TRAITORS + 1):
+        round_dict[i + 1] = {}
+        for j in range(0, NUM_OF_NODES):
+            round_dict[i + 1][j] = []
+
+    for id, node in network.items():
+        for message in node.all_messages:
+            message.reciver_id = id
+            message.is_valid = message in node.valid_messages
+            round_dict[message.round_sent][message.sender_id].append(message)
+
+    return round_dict   
 
 if __name__ == "__main__":
     main()
