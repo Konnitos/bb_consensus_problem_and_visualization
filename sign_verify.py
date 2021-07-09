fed857cc3cdfbd1c46b979efad8560da99dddba9
Zachery.Lange <Zachery.Lange@caveink.com>
2021-07-08 19:44:05 -0500

    Added some print statements to the signature test file

--- sign_verify.py
+++ sign_verify.py
@@ -7,9 +7,14 @@ key = RSA.generate(1024)
 private_key = key.export_key()
 public_key = key.public_key().export_key()
 
+print(f"Private_key : {private_key} \n")
+print(f"Public_key : {public_key} \n")
+
 message = b"ATTACK!"
 hash = SHA256.new(message)
 
 signature = PKCS1_v1_5.new(RSA.import_key(private_key)).sign(hash)
+
+print(signature)
 
 print(PKCS1_v1_5.new(RSA.import_key(public_key)).verify(hash,signature))
